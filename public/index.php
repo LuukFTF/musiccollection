bb1bb856438a9e553045b3b5dac117a6b85eced9
LuukFTF <lucasvdvegt@gmail.com>
2021-03-07 16:56:53 +0100

    first code init

--- public/index.php
+++ public/index.php
@@ -1,2 +1,11 @@
-<?php
-phpinfo();
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <title>Music Collection</title>
+    <script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+</body>
+</html>
