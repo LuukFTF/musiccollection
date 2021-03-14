651884191dd99d1ca81514865e56b7dc91ac6ae5
LuukFTF <lucasvdvegt@gmail.com>
2021-03-14 22:37:55 +0100

    dev

---
+++ public/main/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,
+    initial-schale=1.0">
+    <script type="text/javascript" src="js/main.js"></script>
+    <script src="https://kit.fontawesome.com/0bc22cc72a.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Home | Music Collection</title>
+</head>
+<body>
+
+    <div class="webcontainer">
+        <section class="homepage">
+            <div class="pinned">
+            </div>
+            
+        </section>
+    </div>
+
+    <section class="bottomnavbar-container">
+        <nav class="bottomnavbar container flex nav">
+            <a href="../main" class="btn nav-btn btn-home active"><br><div class="btn-text">Home</div></a>
+            <a href="../settings" class="btn nav-btn btn-settings"><br><div class="btn-text">Settings</div></a>
+        </nav>
+    </section>
+
+</body>
+</html>
+
